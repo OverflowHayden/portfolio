a26b9b43bb939c3f1c7518f5631cee8ec9ffcc65
Hayden Lin <hayden@divtag.nl>
2022-02-17 20:54:55 +0100

    revamp code & update info

---
+++ webpack.config.js
@@ -0,0 +1,132 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+
+const config = {
+    entry: ['./assets/js/src/app.js', './assets/scss/style.scss'],
+    output: {
+        filename: 'assets/js/min/app.min.js',
+        path: __dirname,
+    },
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin(),
+        ],
+    },
+    module: {
+
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            import: true,
+                        },
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(sass|scss)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            import: true,
+                            sourceMap: true,
+                            importLoaders: 2,
+                        },
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: require.resolve('babel-loader'),
+                    options: {
+                        plugins: ['@babel/plugin-transform-runtime'],
+                    },
+                },
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg|webp)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/img/min/[name][ext]',
+                },
+                rules: [
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                            },
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: [0.65, 0.90],
+                                speed: 4,
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            webp: {
+                                quality: 75,
+                            },
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(eot|ttf|woff|woff2)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/fonts/[name][ext]',
+                },
+            },
+        ],
+    },
+    plugins: [
+        new StyleLintPlugin({
+            configFile: '.stylelintrc',
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/css/style.css',
+        }),
+    ],
+};
+
+module.exports = (env, argv) => {
+    if (argv.mode === 'development') {
+        config.devtool = 'source-map';
+    }
+
+    if (argv.mode === 'production') {
+        //...
+    }
+
+    return config;
+};
